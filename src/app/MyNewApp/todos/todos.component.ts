3a811cbf5d88c1d1d98d0e5e603b1131f825df10
Suraj Gupta <8700suraj@gmail.com>
2024-05-21 09:02:24 +0530

    day3.2

---
+++ src/app/MyNewApp/todos/todos.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Todo } from '../../Todo';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css'
+})
+export class TodosComponent {
+  todos: Todo[] = [];
+constructor() {
+  this.todos = [
+    {
+      sno:1,
+      title: "This is title1",
+      desc: "Description1",
+      active: true
+    },
+    {
+      sno:2,
+      title: "This is title2",
+      desc: "Description2",
+      active: true
+    },
+    {
+      sno:3,
+      title: "This is title3",
+      desc: "Description3",
+      active: true
+    }
+  ]
+}
+}
+ 
